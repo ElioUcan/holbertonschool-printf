e093bf0d5d98781860b2a1d938fe5a204c9b7081
Elio Ucan <eliozapata2006@gmail.com>
2026-07-26 19:34:50 -0600

    commit

--- number_char.c
+++ number_char.c
@@ -32,7 +32,7 @@ for (i = 0; format[i] != '\0'; i++)
             str = va_arg(args, char *);
             if (str == NULL)
             {
-            return (-1);
+            return (NULL);
             }
             for (j = 0; str[j] != '\0'; j++)
             {       
